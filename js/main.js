f71216e9e98787075ee4cce2f0cf87ffba071a2d
enthios <trey92094@yahoo.com>
2023-07-25 16:27:43 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function getRandomWordFromArray(wordArray) {
+
+    console.log(getRandomWordFromArray(arr))
+
+    const words = ['Stars', 'Nebula', 'Galaxy'];
+
+    const randomWord = getRandomWordFromArray(words);
+    console.log(randomWord);
+
+
+
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+
+
+    return wordArray[randomIndex];
+}
+
+
+
+
+
+document.body.style.backgroundImage = 'url("Images/backgroundmain.jpeg")';
+document.body.style.backgroundSize = "100%";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundPosition = "auto";
+
+document.h1.style.color = "white";
